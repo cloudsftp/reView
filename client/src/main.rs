be3ae6d16fb911dba402cb8da4e6efba0bcc9d57
Fabian Weik <fabian.weik@protonmail.com>
2026-01-07 20:46:25 +0100

    build gstreamer pipeline with bindings

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,24 +2,24 @@ use std::{io::Read as _, net::TcpStream, thread::sleep, time::Duration};
 
 use anyhow::{Context, Error};
 use async_ssh2_tokio::{AuthMethod, Client, ServerCheckMethod};
+use gstreamer_app::AppSrc;
 use lz4_flex::frame::FrameDecoder;
-use tokio::{
-    sync::mpsc::{self, Receiver},
-};
+use tokio::sync::mpsc::{self, Receiver};
 use tokio_util::io::SyncIoBridge;
 use tracing::{debug, error, info};
 
-use gstreamer::prelude::*;
-use gstreamer_video::prelude::*;
+use gstreamer::{Pipeline, prelude::*};
+use gstreamer_video::{VideoFormat, prelude::*};
 
 const IP: &str = "192.168.2.118";
 const SSH_PORT: u16 = 22;
 const PORT: u16 = 6680;
 
-const HEIGHT: usize = 1872;
-const WIDTH: usize = 1404;
+const HEIGHT: u32 = 1872;
+const WIDTH: u32 = 1404;
 const PIXEL_FORMAT: &str = "bgra";
-const BYTES_PER_PIXEL: usize = 4;
+const VIDEO_FORMAT: VideoFormat = VideoFormat::Bgrx;
+const BYTES_PER_PIXEL: u32 = 4;
 const FILE: &str = ":mem:";
 const SKIP_OFFSET: usize = 2629636;
 
@@ -56,34 +56,53 @@ async fn gstreamer_thread() -> Result<(), Error> {
     sleep(Duration::from_millis(100));
 
     info!("setting up TCP connection");
-    let tcp_input_stream = TcpStream::connect(format!("{}:{}", IP, PORT))
+    let encoded_video_data = TcpStream::connect(format!("{}:{}", IP, PORT))
         .context("could not connect to TCP stream")?;
 
-    let mut decoder = FrameDecoder::new(tcp_input_stream);
+    let mut decoded_video_data = FrameDecoder::new(encoded_video_data);
 
-    // TODO: rewrite in bindings?
-    let pipeline = gstreamer::parse::launch(&format!(
-        "appsrc name={} is-live=true format=time ! rawvideoparse width={} height={} format={} ! videoconvert ! autovideosink",
-        APP_SOURCE_NAME, WIDTH, HEIGHT, PIXEL_FORMAT,
-    )).context("could not build gstreamer pipeline")?;
+    let (pipeline, appsrc) = build_pipeline().context("could not build gstreamer pipeline")?;
+    pipeline.set_state(gstreamer::State::Playing).context("could not start playing gstreamer pipeline")?;
 
-    let pipeline = pipeline.dynamic_cast::<gstreamer::Pipeline>().unwrap();
-    let app_source = pipeline
-        .by_name(APP_SOURCE_NAME)
-        .unwrap()
-        .dynamic_cast::<gstreamer_app::AppSrc>()
-        .unwrap(); // TODO: don't depend on dyanamic cast
-
-    pipeline.set_state(gstreamer::State::Playing).unwrap();
-
-    let mut chunk = vec![0u8; BYTES_PER_PIXEL * HEIGHT * WIDTH];
-
+    let mut chunk = vec![0u8; (BYTES_PER_PIXEL * HEIGHT * WIDTH) as usize];
     loop {
-        decoder.read_exact(&mut chunk).context("could not read from TCP stream")?;
+        decoded_video_data
+            .read_exact(&mut chunk)
+            .context("could not read from TCP stream")?;
 
         let buffer = gstreamer::Buffer::from_mut_slice(chunk.clone());
-        app_source.push_buffer(buffer).context("could not push buffer to app source")?;
+        appsrc
+            .push_buffer(buffer)
+            .context("could not push buffer to app source")?;
     }
+}
+
+fn build_pipeline() -> Result<(Pipeline, AppSrc), Error> {
+    let video_info = gstreamer_video::VideoInfo::builder(VIDEO_FORMAT, WIDTH, HEIGHT)
+        .build()
+        .context("could not build video info")?;
+
+    let appsrc = gstreamer_app::AppSrc::builder()
+        .caps(
+            &video_info
+                .to_caps()
+                .context("could not get caps from video info")?,
+        )
+        .is_live(true)
+        .format(gstreamer::Format::Time)
+        .build();
+
+    let videoconvert = gstreamer::ElementFactory::make("videoconvert").build()?;
+    let sink = gstreamer::ElementFactory::make("autovideosink").build()?;
+
+    let pipeline = gstreamer::Pipeline::default();
+    pipeline
+        .add_many([appsrc.upcast_ref(), &videoconvert, &sink])
+        .context("could not add elements to pipeline")?;
+    gstreamer::Element::link_many([appsrc.upcast_ref(), &videoconvert, &sink])
+        .context("could not link elements toghether")?;
+
+    Ok((pipeline, appsrc))
 }
 
 async fn receive_output(stdout: &mut Receiver<Vec<u8>>) -> Result<String, Error> {
