07d7bbaf36afaebc88d68543062e1d70cb213711
Fabian Weik <fabian.weik@protonmail.com>
2026-01-10 11:00:09 +0100

    fix building server with dagger

--- dagger/main.go
+++ dagger/main.go
@@ -1,26 +1,11 @@
-// A generated module for ReView functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
-	//	"context"
 	"dagger/re-view/internal/dagger"
 )
 
 const (
-	RustVersion = "1.91"
+	RustVersion = "1.92"
 
 	ToltecImage   = "ghcr.io/toltec-dev/rust"
 	ToltecVersion = "v4.0"
@@ -49,6 +34,8 @@ func linuxContainer(source *dagger.Directory) *dagger.Container {
 			"libgstreamer1.0-dev",
 			"libgstreamer-plugins-base1.0-dev",
 		}).
+
+		// Sources
 		WithDirectory("/source", source).
 		WithWorkdir("/source").
 
@@ -61,23 +48,23 @@ func linuxContainer(source *dagger.Directory) *dagger.Container {
 func (m *ReView) BuildServer(source *dagger.Directory) *dagger.File {
 	return toltecContainer(source).
 		WithExec([]string{
-			"cargo", "build", "--release",
-			"--bin", "review-server",
-			"--target", RemarkableTarget,
+			"bash", "-c",
+			"source /opt/x-tools/switch-arm.sh; " +
+				"cargo build --release --bin review-server --target " + RemarkableTarget,
 		}).
-		// WithExec([]string{"cp", "target/" + RemarkableTarget + "/release/review-server", "review-server"}).
-		// File("review-server")
-		File("target/" + RemarkableTarget + "/release/review-server")
+		WithExec(
+			[]string{"cp", "target/" + RemarkableTarget + "/release/review-server", "review-server"},
+		).
+		File("review-server")
 }
 
 func toltecContainer(source *dagger.Directory) *dagger.Container {
 	return dag.Container().
 		From(ToltecImage+":"+ToltecVersion).
+
+		// Sources
 		WithDirectory("/source", source).
 		WithWorkdir("/source")
 
-	// Cache
-	// WithMountedCache("/cache/cargo", dag.CacheVolume("rust-packages-toltec")).
-	// WithEnvVariable("CARGO_HOME", "/cache/cargo").
-	// WithMountedCache("target", dag.CacheVolume("rust-target-toltec"))
+	// Sadly caching breaks compile :(
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Error;
+use tracing::info;
 
 fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+
+    info!("Hello from the server");
+
     Ok(())
 }
